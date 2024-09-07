7835cc350b8a80a3fa92f407690e1796e1d6da40
kamelher <kameleddine.heraguemi@ensia.edu.dz>
2024-09-08 00:53:42 +0100

    adding Change status of the clients duplicate, actiove from the datatabe,

--- resources/views/common/livewire-tables/clients/duplicate.blade.php
+++ resources/views/common/livewire-tables/clients/duplicate.blade.php
@@ -1,3 +1,7 @@
+
+<a wire:click="{{$action}}({{ $recordId }})"
+         onclick="confirm('Are you sure you want to perform this action?') || event.stopImmediatePropagation()">
+
 @if ($status)
 <i style="width: 25px;">
     <svg xmlns="http://www.w3.org/2000/svg" class="inline-block  @if ($successValue === true) text-green @else text-red @endif"  style="width: 25px;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -5,12 +9,13 @@
     </svg>
 
 </i>
-
 @else
     <i style="width: 25px;">
-        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block @if ($successValue === false) text-green @else text-red @endif" style="width: 25px;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block @if ($successValue === false) text-red @else text-green @endif" style="width: 25px;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
     </i>
 
 @endif
+</a>
+
